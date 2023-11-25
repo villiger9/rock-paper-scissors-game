58a77d936bfbfe781f93d501d9de9a10a77217b5
villiger9 <ali.shaker93@gmail.com>
2023-11-25 23:20:52 +0300

    Added comments and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# rock-paper-scissors-game
+# Rock, Paper, Scissors Game
+
+A simple Rock, Paper, Scissors game implemented in HTML, CSS, and JavaScript.
+
+## How to Play
+
+1. Open `index.html` in a web browser.
+2. Click on one of the buttons to make your choice (Rock, Paper, or Scissors).
+3. The computer will randomly choose its move.
+4. The result of the game will be displayed below the buttons.
+
+## Screenshots
+
+Include screenshots or gifs of the game in action.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- [Font Awesome](https://fontawesome.com/) for icons
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 // scripts.js
+// Rock, Paper, Scissors Game
+
 // Example JavaScript using Font Awesome icons
 const myElement = document.getElementById('myElement');
 myElement.innerHTML = '<i class="fas fa-check"></i> Task completed!';
 
+// Function to play the game when a button is clicked
 function playGame(userChoice) {
+    // Array of possible choices
     const choices = ['rock', 'paper', 'scissors'];
+    // Generate a random computer choice
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
+    // Determine the winner
     const result = determineWinner(userChoice, computerChoice);
-
+    // Display the result
     document.getElementById('result').innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}. Result: ${result}`;
 }
 
+// Function to determine the winner of the game
 function determineWinner(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         return 'It\'s a tie!';
